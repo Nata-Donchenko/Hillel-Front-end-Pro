f71089c2a4ea02f7bf8ab73522f73a5198da3d58
Nata Donchenko <n.donchenko91@gmail.com>
2025-10-29 21:38:05 +0200

    HomeTask 4.2.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-// homeTask 4.1.
-let userName = prompt("What is your name?")
-alert(`Hello, ${userName}! How are you?`)
+// homeTask 4.2.
 
+let userNumber = prompt("enter a three-digit number")
+userNumber = userNumber.split("")
+
+if (userNumber[0] === userNumber[1] && userNumber[0] === userNumber[2]) {
+    console.log("all numbers are equal")
+} else if ((userNumber[0] === userNumber[1] || userNumber[0] === userNumber[2]) || (userNumber[1] === userNumber[2] || userNumber[1] === userNumber[0])) {
+    console.log("two numbers are equal")
+} else {
+    console.log("no equal numbers")
+}
