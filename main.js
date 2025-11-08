6a62924af06bc1b72ed6f73a790a723bb00be9e9
Nata Donchenko <n.donchenko91@gmail.com>
2025-11-08 19:04:32 +0200

    HomeTask 6.2

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-// Home task 6.1
-//Створіть об'єкт, що містить інформацію про користувача, таку як ім'я, вік, місце проживання тощо. 
-// Створіть метод об'єкту для отримання та відображення цих даних.
+// Home task 6.2
+// Маєте масив чисел. 
+// Використовуйте вже існуючі методи масиву для створення нового масиву, в якому лише парні числа з оригінального масиву.
 
-let user = {
-  name: 'Nata',
-  age: 34,
-  city: 'Pivdenne',
-  id: 1,
-  getUserInfo () {
-    console.log (`User's name is ${this.name}`)
-    console.log (`User's age is ${this.age}`)
-    console.log (`User is living in ${this.city}`)
-    console.log (`User's id is ${this.id}`)
-  }
-}
+let numArray = [2, 5, 456, 84, 4, 7, 539, 79, 32]
+let numPairedArray = numArray.filter((num) => num % 2 === 0) 
 
-user.getUserInfo()
+console.log(numPairedArray)
