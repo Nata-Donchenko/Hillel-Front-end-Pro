a8d968be0bfd909aff82948a62b6cddf4d95273d
Nata Donchenko <n.donchenko91@gmail.com>
2025-11-16 17:47:25 +0200

    HomeTask 7.2

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
-// Home task 7.1
-// Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 
-// 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
-// Вихідний рядок та символи для видалення задає користувач.
+// Home task 7.2
+// Дано масив з елементами різних типів. 
+// Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
 
-const phrase = prompt('Enter a phrase')
-const letters = prompt('Enter the letters, which you want to delete')
+const someArray = ['a', 258, '90', {name: 'nata', id: 9}, true, 'b', 5, "2", false]
 
-// функція, щоб перевести строку у масив
-function createArray(str) {
-  return str.split('')
-}
-
-// функція, щоб прибрати елементи масиву, що є пробілом.
-// Для того, якщо користувач введе дві або більше літер через пробіл, щоб основна функція 
-// їх не прибирала із основної фрази і в результаті пробіли залишились
-function removeSpaces(arr) {
+function filterNumbers (arr) {
   const result = []
-  arr.filter((el) => { // філтруємо масив
-    if (el !== ' ') {
-      result.push(el.toLowerCase()) // якщо елемент не дорівнює пробілу, то додаємо його у масив у нижньому регістрі, що б можна було видаляти як великі так і маленькі літери
+  arr.forEach((el) => {
+    if (typeof el === 'number' || typeof el === 'string') {
+      if (!isNaN(+el)) {
+        result.push(+el)
+      }
     }
   })
   return result
 }
 
-// основна функція по дз, видаляти можна скільки завгодно літер
-function removeLetters (promptPhrase, promptLetters) { // передаємо два параметри
-const phraseArray = createArray(promptPhrase) // переводимо основну фразу у массив щоб можна було його перебирати
-console.log(phraseArray)
-const lettersArray = removeSpaces(createArray(promptLetters)) // переводимо задані літери у масив та прибираємо пробіли
-console.log(lettersArray)
-const result = phraseArray.filter((el) => !lettersArray.includes(el.toLowerCase())) // перебираємо і фільтруємо масив з фразою на предмет того, що залишаються ті літери, які не містяться у масиві із заданими літерами. Перевірку робимо у нижньому регістрі, що дозволить приьирати і великі і маленькі літери
-return result.join('') // переводимо масив у строку
+function averageNum (arr) {
+  const numbersArray = filterNumbers(arr)
+  const sum = numbersArray.reduce((acc, el) => (acc + el) ,0)
+  return sum / numbersArray.length
 }
 
-console.log(removeLetters(phrase, letters))
+console.log(averageNum(someArray))
