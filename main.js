81d945f9af5ff9655d01b7fd0b85f97077a927aa
Nata Donchenko <n.donchenko91@gmail.com>
2025-11-19 20:53:22 +0200

    HomeTask 8.2. Карринг

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
 
-// Home task 8.1 Замикання
-// Написати функцію, яка приймає 1 параметр. 
-// Та скадае значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
-// console.log(sum(4)); // 4
-// console.log(sum(6)); // 10
-// console.log(sum(10)); // 20
-// console.log(sum(7)); // 27
+// Home task 8.2 Каррінг
+// Створити функцію для розрахунку добутку двох чисел, що викликається так: name(5)(2).
+//  Функція повинна повертати результат (у середині функції не має бути консоль лога!)
 
-function getTotalSum() {
-  let totalSum = 0
-
-  function getSum(x) {
-    totalSum += x
-    return totalSum
+function getMultiplication(x) {
+  return function(y) {
+    return x * y
   }
-
-  return getSum
 }
 
-const sum = getTotalSum()
-
-console.log(sum(4))
-console.log(sum(6))
-console.log(sum(10))
-console.log(sum(7))
+const result = getMultiplication(5)(2)
+console.log(result)
