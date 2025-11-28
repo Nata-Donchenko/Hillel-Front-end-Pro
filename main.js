41feec6dc44df1cd53f3b8f3fcdc6e57dec1c6f4
Nata Donchenko <n.donchenko91@gmail.com>
2025-11-28 23:11:06 +0200

    HomeTask 10.1

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
-// 10.1 Дізнатись суму всіх зарплат користувачів:
-// Об'єкт може містити невідому кількість департаментів та співробітників
+// 10.1 Вивести таблицю Піфагора (10×10), 
+// таблиця повинна бути створена динамічно
 
-let company = {
-  sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
-  development: {
-    web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1000}],
-    internals: [{name: 'Jack', salary: 1300}],
-    lawyers: {
-      firstDepartment: [{name: 'Ann', salary: 2000}, {name: 'Kate', salary: 2000}],
-      secondDepartment: [{name: 'Steve', salary: 1100}],
+const body = document.querySelector('body')
+const wrapper = document.createElement('div')
+body.appendChild(wrapper)
+
+const wrapperTags = ['p', 'h1', 'table']
+wrapperTags.forEach((tag) => {
+  const newTag = document.createElement(tag)
+  wrapper.appendChild(newTag)
+})
+
+const taskNumber = document.querySelector('p')
+taskNumber.textContent = 'Home task 10.1'
+
+const taskName = document.querySelector('h1')
+taskName.textContent = "Table of Pythagoras"
+taskName.style.textAlign = 'center'
+
+const table = document.createElement('table')
+table.style.cssText = `
+  background: #03bfaf80;
+  margin: 0 auto;
+  text-aline: center
+`
+wrapper.appendChild(table)
+
+for (let i = 1; i <= 10; i++) {
+  const line = document.createElement('tr')
+  table.appendChild(line)
+
+  if (i === 1) {
+    line.style.color = '#38aea5ff'
+    line.style.fontWeight = 'bold'
+    line.style.background = 'black'
+  }
+
+  for (let j = 1; j <=10; j++) {
+    const cell = document.createElement('td')
+    cell.style.cssText = `
+      padding: 10px; 
+      font-size: 20px; 
+      text-align: center;
+      border: 1px solid black
+    `
+    cell.textContent = i * j
+    line.appendChild(cell)
+    
+    if (j === 1) {
+      cell.style.color = '#38aea5ff'
+      cell.style.fontWeight = 'bold'
+      cell.style.background = 'black'
+    } 
+    
+    if (i === j && i != 1) {
+      cell.style.fontWeight = 'bold'
+      cell.style.background = '#21787188'
     }
   }
 }
-
-function getTotalSalary(obj) {
-  if(Array.isArray(obj)) {
-    return obj.reduce((sum, worker) => sum + worker.salary, 0)
-  }
-
-  let total = 0
-
-  Object.keys(obj).forEach((key) => total += getTotalSalary(obj[key]))
-
-  return total
-}
-
-console.log(getTotalSalary(company))
