2d1722c6fb8c26f4062143aac324d5e6b64624c1
Nata Donchenko <n.donchenko91@gmail.com>
2025-11-29 22:35:09 +0200

    HomeTask 11.3

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-// HomeTassk 11.2. Кнопка зміни кольору
-//Є блок із текстом на сторінці та кнопка. 
-// При натисканні на кнопку текст змінює колір. 
-// При повторному натисканні – повертається попередній колір
+// HomeTask 11.3. Отримання випадкового зображення
+// Покласти в папку будь-які зображення 
+// 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. 
+// Вивести зображення, отримане випадковим чином (Math.random)
 
 const body = document.querySelector('body')
 
-const someText = document.createElement('p')
-someText.textContent = 'Home task 11.2'
-someText.classList.add('text')
+const container = document.createElement('div')
+container.classList.add('container')
 
-const someButton = document.createElement('button')
-someButton.textContent = 'Button'
-someButton.classList.add('btn')
+body.appendChild(container)
 
-body.appendChild(someText)
-body.appendChild(someButton)
+const button = document.createElement('button')
+button.classList.add('btn')
+button.textContent = 'Click for a random image'
+container.appendChild(button)
 
-function changeColor() {
-  someButton.classList.toggle('btn-change-color')
-  someText.classList.toggle('text-change-color')
+const image = document.createElement('img')
+container.appendChild(image)
+
+const images = []
+for (let i = 1; i <= 9; i++) {
+  images.push(`images/${i}.jpg`)
 }
 
-someButton.addEventListener('click', changeColor)
+function getImage () {
+  let randomNumber = Math.floor(Math.random() * images.length)
+  image.src = images[randomNumber]
+  image.classList.add('img')
+}
+
+button.addEventListener('click', getImage)
