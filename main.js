76d76ecd9377e983ba2ab1c4889b6ed1f11806a7
Nata Donchenko <n.donchenko91@gmail.com>
2025-12-11 21:59:38 +0200

    HomeTask 12.3

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-// ДЗ 12.2. Використання подій
-// Створіть HTML-сторінку з декількома кнопками. 
-// Ваше завдання - створити обробник подій для батьківського 
-// елементу, який відслідковуватиме кліки на всіх кнопках.
+// ДЗ 12.3. TODO лист
+// Створіть HTML-сторінку, яка містить список завдань (to-do list) з можливістю додавання нових завдань. 
+// Ваше ціль - використовуючи делегування подій, створити обробник подій для списку завдань, 
+// який дозволить видаляти завдання при кліку на них.
 
 // Покроковий план:
 
-// 1. Створіть HTML-елементи: батьківський контейнер 
-// з декількома вкладеними кнопками.
-
-// 2. Додайте обробник подій до батьківського контейнера, 
-// який відслідковуватиме кліки.
-
-// 3. Після кліку на будь-якій кнопці, 
-// виведіть повідомлення про те, яка саме кнопка була клікнута.
+// Створіть HTML-елементи: список завдань ul, текстове поле для вводу нових завдань та кнопку для додавання.
+// Додайте обробник подій до списку завдань ul, використовуючи делегування.
+// При кліку на будь-якій кнопці видалення, видаліть цей пункт.
+// Додайте можливість вводити нові завдання у текстове поле і додавати їх до списку за допомогою кнопки.
 
 const body = document.querySelector('body')
 
@@ -20,17 +16,52 @@ const container = document.createElement('div')
 container.classList.add('container')
 body.appendChild(container)
 
-for (let i = 1; i <=3; i++) {
-  const btn = document.createElement('button')
-  btn.classList.add('btn')
-  btn.textContent = `Button ${i}`
-  container.appendChild(btn)
+const title = document.createElement('h1')
+title.textContent = 'My tasks'
+
+const inputDiv = document.createElement('div')
+inputDiv.classList.add('input-wrapper')
+
+const list = document.createElement('ul')
+
+container.append(title, inputDiv, list)
+
+const input = document.createElement('input')
+input.type = 'text'
+input.placeholder = 'Enter a task'
+
+const addBtn = document.createElement('button')
+addBtn.classList.add('addBtn')
+addBtn.textContent = 'Add'
+
+inputDiv.append(input, addBtn)
+
+
+function addTask () {
+  const taskText = input.value
+
+  if (taskText === '') {
+    return
+  }
+
+  const task = document.createElement('li')
+  task.textContent = taskText
+
+  const removeBtn = document.createElement('button')
+  removeBtn.classList.add('removeBtn')
+  removeBtn.textContent = 'Delete'
+
+  list.appendChild(task)
+  task.appendChild(removeBtn)
+
+  input.value = ''
 }
 
-function getBtnNum (event) {
+function removeTask (event) {
   if (event.target.tagName === 'BUTTON') {
-    alert(`Clicked ${event.target.textContent}`)
+    event.target.parentNode.remove()
   }
 }
 
-container.addEventListener('click', getBtnNum)
+addBtn.addEventListener('click', addTask)
+list.addEventListener('click', removeTask)
