2b8f286e39c7baae4802feb813aed72de41a9bba
Nata Donchenko <n.donchenko91@gmail.com>
2026-01-14 22:08:34 +0200

    HomwTasks 17.1, 17.2, 17.3

--- js/main.js
+++ js/main.js
@@ -1,141 +1,90 @@
-// 16.1 Вам потрібно зробити конструктор сутності "Студент". 
-// Студент має ім'я, прізвище, рік народження — це властивості. 
-// Є масив з оцінками, це також властивість. 
-// І є можливість отримати вік студента та його середній бал – це методи.
+// 17.1. Клас "Калькулятор"
+// Створіть клас Calculator, який буде мати методи для виконання базових математичних 
+// операцій: додавання, віднімання, множення та ділення.
 
-// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, 
-// спочатку він не заповнений, але на 25 елементів. 
-// Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо 
-// метод .present() на чергове порожнє місце, в масив записується true, 
-// коли викликаємо .absent() - записується false. 
-// Передбачте будь-який захист від того, щоб у масиві відвідуваності 
-// не могло бути більше 25 записів. 
-// Масив – це властивість, present та absent – методи.
-
-// Останній метод: .summary(), перевіряє середню оцінку 
-// і середнє відвідування(кількістьВідвідин/кількістьЗанять), 
-// і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, 
-// о метод summary повертає рядок "Молодець!", якщо одне з цих значень менше , 
-// то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
-
-// Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри 
-// (конкретних студентів) і показати використання цих методів.
-
-function Student (name, surname, birthYear, grades) {
-  this.name = name
-  this.surname = surname
-  this.birthYear = birthYear
-  this.grades = grades
-  this.attendance = new Array(25).fill(null) 
-}
-
-Student.prototype.getAge = function () {
-  const currentYear = new Date().getFullYear()
-  return currentYear - this.birthYear
-}
-
-Student.prototype.getAverageGrade = function () {
-  if(this.grades.length === 0) {
-    return 0
+class Calculator {
+  add(a, b) {
+    return a + b
   }
 
-  const sum = this.grades.reduce((acc, grade) => acc + grade, 0)
-  return sum / this.grades.length
-}
-
-Student.prototype._getAttendanceIndex = function () {
-  return this.attendance.indexOf(null)
-}
-
-Student.prototype.present = function () {
-  const index = this._getAttendanceIndex()
-  if(index === -1) {
-    return
+  subtract(a, b) {
+    return a - b
   }
 
-  this.attendance[index] = true
-}
-
-Student.prototype.absent = function () {
-  const index = this._getAttendanceIndex()
-  if(index === -1) {
-    return
+  multiply(a, b) {
+    return a * b
   }
 
-  this.attendance[index] = false
-}
-
-Student.prototype.summary = function () {
-  const averageGrade = this.getAverageGrade()
-
-  const visited = this.attendance.filter((visit) => visit === true).length
-  const total = this.attendance.filter((visit) => visit !== null).length
-  let totalAttendance = 0
-
-  if(total !== 0) {
-    totalAttendance = visited / total
-  }
-
-  if(averageGrade > 90 && totalAttendance > 0.9) {
-    return 'Молодець!'
-  } else if (averageGrade > 90 || totalAttendance > 0.9) {
-    return 'Добре, але можна краще'
-  } else {
-    return 'Редиска!'
+  divide(a, b) {
+    return a / b
   }
 }
 
-const firstStudent = new Student('Jack', 'Adams', 1991, [96, 90, 99])
-const secondStudent = new Student('Ann', 'Philips', 1995, [85, 64, 70])
-const thirdStudent = new Student('Alex', 'Black', 1993, [24, 12, 45])
+const calc = new Calculator()
+console.log(calc.add(5, 3))
+console.log(calc.subtract(10, 4))
+console.log(calc.multiply(3, 6))
+console.log(calc.divide(8, 2))
 
-function changeNull (arr) {
-  arr.forEach((el, index, arr) => {
-    if(el === null) {
-      arr[index] = false
+
+// 17.2. Клас "Тренер"
+// Створіть клас Coach, який буде представляти тренера. 
+// Додайте властивості, такі як ім'я, спеціалізація та рейтинг. 
+// Також реалізуйте метод для виведення інформації про тренера та його рейтинг.
+
+class Coach {
+  constructor (name, specialization, rating) {
+    this.name = name
+    this.specialization = specialization
+    this.rating = rating
+  }
+
+  displayInfo () {
+    console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`)
+  }
+}
+
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+
+coach1.displayInfo()
+coach2.displayInfo()
+
+
+// 17.3. Клас "Банківський рахунок"
+// Створіть клас BankAccount, який буде представляти банківський рахунок. 
+// Додайте властивість балансу та методи для внесення та зняття грошей.
+
+class BankAccount {
+  #balance
+
+  constructor (balance) {
+    this.#balance = balance
+  }
+
+  deposit(value) {
+    return this.#balance += value
+  }
+
+  withdraw(value) {
+    if(value > this.#balance) {
+      console.log("You don't have a money")
+      return
     }
-  })
+
+    this.#balance -= value
+  }
+
+  getBalance() {
+    return this.#balance
+  }
 }
 
-for (let i = 0; i < 24; i++) {
-  firstStudent.present()
-}
+const account1 = new BankAccount(1000);
+console.log(account1.getBalance()); 
 
-changeNull(firstStudent.attendance)
-console.log(firstStudent)
-console.log(`
-  name: ${firstStudent.name}
-  surname: ${firstStudent.surname}
-  age: ${firstStudent.getAge()}
-  average rating: ${firstStudent.getAverageGrade()}
-  grade: ${firstStudent.summary()}
-`)
+account1.deposit(500);
+console.log(account1.getBalance()); 
 
-
-for (let i = 0; i < 23; i++) {
-  secondStudent.present()
-}
-
-changeNull(secondStudent.attendance)
-console.log(secondStudent)
-console.log(`
-  name: ${secondStudent.name}
-  surname: ${secondStudent.surname}
-  age: ${secondStudent.getAge()}
-  average rating: ${secondStudent.getAverageGrade()}
-  grade: ${secondStudent.summary()}
-`)
-
-for (let i = 0; i < 10; i++) {
-  thirdStudent.present()
-}
-
-changeNull(thirdStudent.attendance)
-console.log(thirdStudent)
-console.log(`
-  name: ${thirdStudent.name}
-  surname: ${thirdStudent.surname}
-  age: ${thirdStudent.getAge()}
-  average rating: ${thirdStudent.getAverageGrade()}
-  grade: ${thirdStudent.summary()}
-`)
+account1.withdraw(200);
+console.log(account1.getBalance());
