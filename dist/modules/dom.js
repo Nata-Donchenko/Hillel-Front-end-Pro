a86ed9488fa0d589e5e375fd25dfe90613046b91
Nata Donchenko <n.donchenko91@gmail.com>
2026-02-05 14:30:43 +0200

    HomeTask 22.1

--- dist/modules/dom.js
+++
@@ -1,6 +0,0 @@
-export var refs = {
-  todoForm: document.querySelector('.todoForm'),
-  list: document.querySelector('.todoList'),
-  modal: document.querySelector('#deleteModal'),
-  confirmDeleteBtn: document.querySelector('#confirmDeleteBtn')
-};
---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+// ДЗ 22.1. Білд через Webpack
+// На основі одного з попередніх завдань зробити білд через Webpack. Що має робити білд:
+// Оптимізація, конкатинація, мініфікація JS
+// Компіляція SCSS в CSS. Мініфікація CSS
+// Вотчер
+// За бажанням:
+
+// Dev-server
+// image-minimizer-webpack-plugin
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../style.scss'
+
+import { refs } from './modules/dom.js'
+import { onSubmit } from './modules/todoForm.js'
+import { onClickByList, addInitList } from './modules/todoList.js'
+import { updateTimers } from './modules/deadline.js'
+
+refs.todoForm.addEventListener('submit', onSubmit)
+refs.list.addEventListener('click', onClickByList)
+
+addInitList()
+setInterval(updateTimers, 1000)
+updateTimers()
